b4eac6269a8f7cb7713c71e6c5ee11c2835ec2fe
Zachary Seguin <development@zacharyseguin.ca>
2015-01-07 18:55:30 -0500

    Allow for LDAP authentication when using API where CAS authentication is enabled

--- plugins/auth_cas/init.php
+++ plugins/auth_cas/init.php
@@ -36,6 +36,25 @@ class Auth_CAS extends Plugin implements IAuthModule {
 
     function authenticate($login, $password) {
         if (!$this->initialized) $this->init_phpcas();
+        if (!empty($login) && !empty($password) && defined('AUTH_CAS_USE_LDAP_FOR_API') && AUTH_CAS_USE_LDAP_FOR_API) {
+           $ldap = ldap_connect(AUTH_CAS_LDAP_HOST);
+
+           if (!$ldap) return false;
+
+           ldap_set_option($ldap, LDAP_OPT_PROTOCOL_VERSION, 3);
+           ldap_set_option($ldap, LDAP_OPT_REFERRALS, 0);
+
+           $search_result = ldap_search($ldap, AUTH_CAS_LDAP_SEARCH_BASE, str_replace("$1", $login, AUTH_CAS_LDAP_SEARCH_PARAM), array('dn'));
+           if ($search_result === FALSE) return false;
+
+           $user_results = ldap_get_entries($ldap, $search_result);
+           if ($user_results['count'] === 0) return false;
+
+           $bind_result = ldap_bind($ldap, $user_results[0]['dn'], $password);
+           if ($bind_result) return $this->base->auto_create_user($login, $password);
+
+           return false;
+        }// end of if
 
         phpCAS::handleLogoutRequests();
         phpCAS::forceAuthentication();
